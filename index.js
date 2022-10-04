f9832f7863cf3a3a6d578c3076257c4e92a966c4
David Teo <starrynightskyhoney@gmail.com>
2022-10-04 15:33:40 +0100

    Adding dotenv, postgres integration

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require("express")
+const express = require("express");
+const schedule = require('node-schedule');
 const app = express();
 
-const { Client } = require('pg');
-const client = new Client();
-console.log(client);
-client.connect();
-// client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-//   console.log(err ? err.stack : res.rows[0].message) // Hello World!
-//   client.end()
-// })
+const { Pool } = require('pg');
+require('dotenv').config();
 
-// make index.html accessible to clients
+const pool = new Pool({
+    host: 'localhost',
+    user: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: 'vocabdb',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
 app.use(express.static('public'))
 
 app.get('/', function(request, response){
@@ -18,3 +22,24 @@ app.get('/', function(request, response){
 });
 
 app.listen(8000);
+
+schedule.scheduleJob('*/10 * * * * *', function(){
+    console.log('Every half minute');
+
+    getVocabEntries();
+});
+
+function getVocabEntries() {
+    pool.connect((err, client, release) => {
+        if(err) {
+            return console.error('Error acquiring client', err.stack)
+        }
+        client.query('SELECT dutch, english, notes, pronunciationLink, seen, mastered FROM vocabulary', (err, result) => {
+            release();
+            if(err) {
+                return console.error('Error executing query', err.stack);
+            }
+            console.log(result.rows);
+        });
+    });
+}
