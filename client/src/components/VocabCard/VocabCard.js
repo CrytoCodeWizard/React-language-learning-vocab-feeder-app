5951c8fe5efb2300ff207c152d8677e4255b0906
David Teo <starrynightskyhoney@gmail.com>
2022-11-14 15:45:41 +0000

    shifting back to spaces (2) and refactoring Vocab Card css to different file

--- client/src/components/VocabCard/VocabCard.js
+++ client/src/components/VocabCard/VocabCard.js
@@ -1,32 +1,31 @@
 import React from "react";
-import "./../../App.css";
+import "./VocabCard.css";
 
 const VocabCard = (props) => {
-    return (
-        <div className="vocab-card-wrapper">
-            <div className="vocab-card">
-                <div className={props.css.frontCSS}>
-                    <a href={props.card.pronunciationlink} target="_blank">
-                        <p className="vocab-card-text">{props.card.dutch}</p>
-                    </a>
-                </div>
-                <div className={props.css.backCSS}>
-                    <p className="vocab-card-text">{props.card.english}</p>
-                </div>
-            </div>
-            <div className="vocab-card-controls">
-                <div className="vocab-card-controls-flip">
-                    <button onClick={props.FlipCard} className="vocab-card-controls-flip-btn">Flip</button>
-                </div>
-                <br className="vocab-card-controls-spacer" />
-                <div className="vocab-card-controls-nav">
-                    <button onClick={props.GetPrevCard} className={props.css.prevCSS}>❮ Prev</button>
-                    <button onClick={props.GetNextCard} className={props.css.nextCSS}>Next ❯</button>
-                </div>
-            </div>
+  return (
+    <div className="vocab-card-wrapper">
+      <div className="vocab-card">
+        <div className={props.css.frontCSS}>
+          <a href={props.card.pronunciationlink} target="_blank">
+            <p className="vocab-card-text">{props.card.dutch}</p>
+          </a>
         </div>
-
-    );
+        <div className={props.css.backCSS}>
+          <p className="vocab-card-text">{props.card.english}</p>
+        </div>
+      </div>
+      <div className="vocab-card-controls">
+        <div className="vocab-card-controls-flip">
+          <button onClick={props.FlipCard} className="vocab-card-controls-flip-btn">Flip</button>
+        </div>
+        <br className="vocab-card-controls-spacer" />
+        <div className="vocab-card-controls-nav">
+          <button onClick={props.GetPrevCard} className={props.css.prevCSS}>❮ Prev</button>
+          <button onClick={props.GetNextCard} className={props.css.nextCSS}>Next ❯</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VocabCard;
