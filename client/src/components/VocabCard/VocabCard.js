92cd110efafb23df7ca2288f9ec72edada1e41a3
David Teo <starrynightskyhoney@gmail.com>
2022-11-14 14:26:27 +0000

    adding css and placement for button row

--- client/src/components/VocabCard/VocabCard.js
+++ client/src/components/VocabCard/VocabCard.js
@@ -3,20 +3,28 @@ import "./../../App.css";
 
 const VocabCard = (props) => {
     return (
-        <div className="vocab-card">
-            <div className={props.css.frontCSS}>
-                <a href={props.card.pronunciationlink} target="_blank">
-                    <p className="vocab-card-text">{props.card.dutch}</p>
-                </a>
+        <div className="vocab-card-wrapper">
+            <div className="vocab-card">
+                <div className={props.css.frontCSS}>
+                    <a href={props.card.pronunciationlink} target="_blank">
+                        <p className="vocab-card-text">{props.card.dutch}</p>
+                    </a>
+                </div>
+                <div className={props.css.backCSS}>
+                    <p className="vocab-card-text">{props.card.english}</p>
+                </div>
             </div>
-            <div className={props.css.backCSS}>
-                <p className="vocab-card-text">{props.card.english}</p>
+            <div className="vocab-card-controls">
+                <div className="vocab-card-controls-nav">
+                    <button onClick={props.GetPrevCard} className={props.css.prevCSS} style={{float: 'left'}}>&#8592;</button>
+                    <button onClick={props.GetNextCard} className={props.css.nextCSS} style={{float: 'right'}}>&#8594;</button>
+                </div>
+                <div className="vocab-card-controls-flip">
+                    <button onClick={props.FlipCard}>Flip</button>
+                </div>
             </div>
-
-            <button onClick={props.GetPrevCard} className={props.css.prevCSS}>Prev</button>
-            <button onClick={props.GetNextCard} className={props.css.nextCSS}>Next</button>
-            <button onClick={props.FlipCard}>Flip</button>
         </div>
+
     );
 }
 
