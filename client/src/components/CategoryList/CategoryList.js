b840b5f14b622d003cdf72fbfe881c4c16bd2a2a
David Teo <starrynightskyhoney@gmail.com>
2022-11-14 15:52:33 +0000

    shifting back to spaces (2)

--- client/src/components/CategoryList/CategoryList.js
+++ client/src/components/CategoryList/CategoryList.js
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 import "./../../App.css";
 
 const CategoryList = (props) => {
-    return (
-        <div className="ReviewApp">
-            <h1>Choose a category</h1>
-            <ul>
-                {props.categories.map((category) =>
-                    <li key={category}>
-                        <Link className="category-list-item" to={"/review?set_name=" + category} onClick={props.GetRecordsForCategory}>
-                            {category}
-                        </Link>
-                    </li>
-                )}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="ReviewApp">
+      <h1>Choose a category</h1>
+      <ul>
+        {props.categories.map((category) =>
+          <li key={category}>
+            <Link className="category-list-item" to={"/review?set_name=" + category} onClick={props.GetRecordsForCategory}>
+              {category}
+            </Link>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default CategoryList;
