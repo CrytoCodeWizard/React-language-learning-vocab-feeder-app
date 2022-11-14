d169c091e4d26bb275957e9ac2a1ce03ebbf56f5
David Teo <starrynightskyhoney@gmail.com>
2022-11-14 13:08:01 +0000

    adding vocab-card css wrapper and removing unnecessary <ul>

--- client/src/components/VocabCard/VocabCard.js
+++ client/src/components/VocabCard/VocabCard.js
@@ -3,19 +3,17 @@ import "./../../App.css";
 
 const VocabCard = (props) => {
     return (
-        <div>
-            <ul>
-                <div className={props.css.frontCSS}>
-                    <p className="vocab-card-text">{props.card.dutch}</p>
-                </div>
-                <div className={props.css.backCSS}>
-                    <p className="vocab-card-text">{props.card.english}</p>
-                </div>
+        <div className="vocab-card">
+            <div className={props.css.frontCSS}>
+                <p className="vocab-card-text">{props.card.dutch}</p>
+            </div>
+            <div className={props.css.backCSS}>
+                <p className="vocab-card-text">{props.card.english}</p>
+            </div>
 
-                <button onClick={props.GetPrevCard} className={props.css.prevCSS}>Prev</button>
-                <button onClick={props.GetNextCard} className={props.css.nextCSS}>Next</button>
-                <button onClick={props.FlipCard}>Flip</button>
-            </ul>
+            <button onClick={props.GetPrevCard} className={props.css.prevCSS}>Prev</button>
+            <button onClick={props.GetNextCard} className={props.css.nextCSS}>Next</button>
+            <button onClick={props.FlipCard}>Flip</button>
         </div>
     );
 }
