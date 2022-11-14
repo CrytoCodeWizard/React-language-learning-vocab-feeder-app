f7764f95ade6af946ed214d27089dd96fe59bee7
David Teo <starrynightskyhoney@gmail.com>
2022-11-14 15:51:53 +0000

    shifting back to spaces (2)

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,11 +3,11 @@ import "./../../App.css";
 import WindmillSVG from "./../../windmill.svg";
 
 const Home = (props) => {
-    return (
-        <div className="Windmill-Icon">
-            <img src={WindmillSVG} alt="Windmill SVG" />
-        </div>
-    );
+  return (
+    <div className="Windmill-Icon">
+      <img src={WindmillSVG} alt="Windmill SVG" />
+    </div>
+  );
 }
 
 export default Home;
