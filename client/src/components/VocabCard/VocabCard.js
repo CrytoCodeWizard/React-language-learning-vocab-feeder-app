99e5192990167b7cac0bb119ca5df615e6e5279d
David Teo <starrynightskyhoney@gmail.com>
2022-11-15 13:43:16 +0100

    Fixes #16; refactors main constants into constant.js file

--- client/src/components/VocabCard/VocabCard.js
+++ client/src/components/VocabCard/VocabCard.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./VocabCard.css";
+import * as Constants from "./../../constants";
 
 const VocabCard = (props) => {
   return (
     <div className="vocab-card-wrapper">
       <div className="vocab-card">
         <div className={props.css.frontCSS}>
-          <a href={props.card.pronunciationlink} target="_blank">
+          <a href={props.card.pronunciationlink} target="_blank" rel="noreferrer">
             <p className="vocab-card-text">{props.card.dutch}</p>
           </a>
         </div>
@@ -16,12 +17,12 @@ const VocabCard = (props) => {
       </div>
       <div className="vocab-card-controls">
         <div className="vocab-card-controls-flip">
-          <button onClick={props.FlipCard} className="vocab-card-controls-flip-btn">Flip</button>
+          <button onClick={props.FlipCard} className="vocab-card-controls-flip-btn">{Constants.VOCAB_CARD_FLIP_BUTTON_STR}</button>
         </div>
         <br className="vocab-card-controls-spacer" />
         <div className="vocab-card-controls-nav">
-          <button onClick={props.GetPrevCard} className={props.css.prevCSS}>❮ Prev</button>
-          <button onClick={props.GetNextCard} className={props.css.nextCSS}>Next ❯</button>
+          <button onClick={props.GetPrevCard} className={props.css.prevCSS}>{Constants.VOCAB_CARD_PREV_BUTTON_STR}</button>
+          <button onClick={props.GetNextCard} className={props.css.nextCSS}>{Constants.VOCAB_CARD_NEXT_BUTTON_STR}</button>
         </div>
       </div>
     </div>
---
+++ client/src/constants/index.js
@@ -0,0 +1,34 @@
+// ReviewVocab.js
+export const SETNAME_QUERY_PARAM = "set_name";
+export const GET_REVIEW_CATEGORIES_ENDPOINT = "/getReviewCategories";
+export const GET_VOCAB_FOR_CATEGORY_ENDPOINT = "/getVocabForCategory";
+export const SHOW_CARD_SIDE_CSS = "vocab-card-show-side";
+export const HIDE_CARD_SIDE_CSS = "vocab-card-hide-side";
+export const HIDE_NEXT_BTN_CSS = "vocab-card-hide-next-btn";
+export const HIDE_PREV_BTN_CSS = "vocab-card-hide-prev-btn";
+
+// CategoryList.js
+export const REVIEW_ENDPOINT_SETNAME_PARAM = "/review?" + SETNAME_QUERY_PARAM + "=";
+
+// SlackDailyVocab.js
+export const GET_SLACK_INFO_ENDPOINT = "/getSlackInfo";
+export const SEND_SLACK_MSG_ENDPOINT = "/sendSlack";
+export const SLACK_INPUT_PLACEHOLDER = "# of Records to Send";
+export const SLACK_SENT_CONFIRMATION = "Your vocab has been sent to Slack!";
+export const SLACK_RECORD_MIN = 1;
+export const SLACK_RECORD_MAX = 25;
+
+// VocabCard.js
+export const VOCAB_CARD_PREV_BUTTON_STR = "❮ Prev";
+export const VOCAB_CARD_NEXT_BUTTON_STR = "Next ❯";
+export const VOCAB_CARD_FLIP_BUTTON_STR = "Flip";
+
+// AWS
+export const S3_BUCKET_URL = "https://vocab-feeder.s3.eu-west-3.amazonaws.com/";
+
+// Generic
+export const ERROR_STR = "Error: ";
+export const GET_METHOD = "GET";
+export const POST_METHOD = "POST";
+export const CONTENT_TYPE_JSON_UTF8 = "application/json; charset=UTF-8";
+export const LOADING_STR = "Loading...";
