1ef7a02ca1919434e7c883d887184bd5369295af
David Teo <starrynightskyhoney@gmail.com>
2022-11-23 14:27:00 +0100

    Fixes #9; basic styling for review types screen

--- client/src/components/ReviewType/ReviewType.js
+++ client/src/components/ReviewType/ReviewType.js
@@ -3,11 +3,11 @@ import * as Constants from "./../../constants";
 
 const ReviewVocab = (props) => {
   return (
-    <div>
-      <Link className="category-list-item" to={"/review?set_name=" + props.setName + "&review_type=" + Constants.VOCAB_CARD_REVIEWTYPE_PRACTICE_STR}>
+    <div className="review-type-options">
+      <Link className="review-type-item" to={"/review?set_name=" + props.setName + "&review_type=" + Constants.VOCAB_CARD_REVIEWTYPE_PRACTICE_STR}>
         {Constants.VOCAB_CARD_REVIEWTYPE_PRACTICE_LABEL}
       </Link>
-      <Link className="category-list-item" to={"/review?set_name=" + props.setName + "&review_type=" + Constants.VOCAB_CARD_REVIEWTYPE_TEST_STR}>
+      <Link className="review-type-item" to={"/review?set_name=" + props.setName + "&review_type=" + Constants.VOCAB_CARD_REVIEWTYPE_TEST_STR}>
         {Constants.VOCAB_CARD_REVIEWTYPE_TEST_LABEL}
       </Link>
     </div>
