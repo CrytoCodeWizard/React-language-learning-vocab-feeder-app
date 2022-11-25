c148ebd7473ae392ced3e2e24a26a86987791b3e
David Teo <starrynightskyhoney@gmail.com>
2022-11-25 14:01:59 +0100

    Fixes #22; adds LessonNotes component with css to render HTML pulled from DB

--- client/src/components/LessonNotes/LessonNotes.js
+++ client/src/components/LessonNotes/LessonNotes.js
@@ -1,13 +1,17 @@
 import * as Constants from "../../constants";
 
+import "./LessonNotes.css";
 const LessonNotes = (props) => {
     const notes = props.GetNotes();
 
     return (
         <div className="LessonNotes">
             <h1>{Constants.LESSON_NOTES_TITLE + " (" + notes.person + ")"}</h1>
+            <h2>{notes.lesson_title}</h2>
             <h2>{notes.lesson_date}</h2>
-            <h2>{notes.notes}</h2>
+            <div className="lesson-notes-contents-wrapper">
+                <div className="lesson-notes-contents" dangerouslySetInnerHTML={{__html: notes.notes}} />
+            </div>
         </div>
     );
 }
