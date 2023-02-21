e24276a01a5c91efc3f0853667fd6e575594cf2e
David Teo <starrynightskyhoney@gmail.com>
2023-02-21 20:19:22 +0100

    Fixes #48; shifting App.js to functional component

--- client/src/App.js
+++ client/src/App.js
@@ -4,39 +4,32 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import "./App.css";
 import * as Constants from "./constants";
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {data: ''};
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<BrowserRouter>
-					<header className="App-header">
-						<div>
-							<span>{Constants.APP_HEADER_TITLE}</span>
-							<nav>
-								<ul>
-									{Constants.ROUTES.map((link) =>
-										<li key={link.linkLabel}>
-											<Link to={link.route} className="navAnchor">{link.linkLabel}</Link>
-										</li>
-									)}
-								</ul>
-							</nav>
-						</div>
-					</header>
-					<Routes>
-						{Constants.ROUTES.map((route) =>
-							<Route key={route.element} exact path={route.route} element={route.element}/>
-						)}
-					</Routes>
-				</BrowserRouter>
-			</div>
-		);
-	}
+const App = (props) => {
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<header className="App-header">
+					<div>
+						<span>{Constants.APP_HEADER_TITLE}</span>
+						<nav>
+							<ul>
+								{Constants.ROUTES.map((link) =>
+									<li key={link.linkLabel}>
+										<Link to={link.route} className="navAnchor">{link.linkLabel}</Link>
+									</li>
+								)}
+							</ul>
+						</nav>
+					</div>
+				</header>
+				<Routes>
+					{Constants.ROUTES.map((route) =>
+						<Route key={route.element} exact path={route.route} element={route.element}/>
+					)}
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
