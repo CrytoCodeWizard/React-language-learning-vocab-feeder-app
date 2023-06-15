7e862c27600b6b47a0468ce57c513866ea757c29
David Teo <starrynightskyhoney@gmail.com>
2023-06-15 16:55:19 +0200

    initial commit of add component

--- client/src/components/AddVocab/AddVocab.jsx
+++ client/src/components/AddVocab/AddVocab.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 
 import * as Constants from "./../../constants";
-import styles from "./AddVocab.module.css";
-const AddVocab = ({
-  editForm,
-  handleChange,
-  handleVocabUpdate,
-  handleCancel,
-}) => {
-  let { id, dutch, english, pronunciationlink, notes, set_name } = editForm;
 
-  // PATCH request; calls handleVocabUpdate to push changes to the page
-  const handleEditForm = (e) => {
+const AddVocab = ({
+  addForm,
+  handleAddChange,
+  handleCancel,
+  setAddRecord,
+  onCreateVocab,
+  styles,
+}) => {
+  let { dutch, english, pronunciationlink, notes, set_name } = addForm;
+
+  // PATCH request; calls handleVocabCreate to push changes to the page
+  const handleAddForm = (e) => {
     e.preventDefault();
 
+    console.log("add clicked...");
     fetch(Constants.VOCAB_RECORDS_ENDPOINT, {
-      method: Constants.PATCH_METHOD,
-      body: JSON.stringify(editForm),
+      method: Constants.POST_METHOD,
+      body: JSON.stringify(addForm),
     })
       .then((res) => res.json())
       .then((updatedVocab) => {
-        handleVocabUpdate(updatedVocab);
+        handleVocabCreate(updatedVocab);
       })
       .catch((err) => {
         console.error(Constants.ERROR_STR, err);
       });
+  };
+
+  // when PATCH request happens; auto-hides the form, pushes changes to display
+  const handleVocabCreate = (updatedVocab) => {
+    setAddRecord(false);
+    onCreateVocab(updatedVocab);
   };
 
   const labels = {
@@ -37,18 +46,18 @@ const AddVocab = ({
 
   return (
     <div className={styles.formStyles}>
-      <form onSubmit={handleEditForm}>
+      <form onSubmit={handleAddForm}>
         <div className="form-wrapper">
-          <div className={styles.editFormInputWrapper}>
+          <div className={styles.formInputWrapper}>
             {Object.entries(labels).map(([key, value], i) => (
-              <div className={styles.editFormInput}>
+              <div className={styles.formInput}>
                 <div className={styles.inputLabel}>{key}</div>
                 <div>
                   <input
                     type="text"
                     name={key.toLowerCase()}
                     value={value}
-                    onChange={handleChange}
+                    onChange={handleAddChange}
                   />
                 </div>
               </div>
@@ -56,11 +65,11 @@ const AddVocab = ({
           </div>
         </div>
 
-        <div classNames={styles.editFormBtnRow}>
-          <div className={styles.editFormBtns}>
+        <div classNames={styles.formBtnRow}>
+          <div className={styles.formBtns}>
             <button type="submit">Save</button>
           </div>
-          <div className={styles.editFormBtns}>
+          <div className={styles.formBtns}>
             <button onClick={handleCancel}>Cancel</button>
           </div>
         </div>
